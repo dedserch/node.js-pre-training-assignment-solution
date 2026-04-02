47a526f95349ea82ecddb7be764aefd2ec446b6f
dedserch <dedmemchik@gmail.com>
2026-04-03 01:49:56 +0300

    Add: solution for task 1

--- JS-TS/solutions/types.ts
+++ JS-TS/solutions/types.ts
@@ -1,9 +1,17 @@
 interface Todo {
-
+    id: number;
+    title: string;
+    description?: string;
+    status?: TodoStatus;
+    readonly createdAt: Date;
 }
 
 enum TodoStatus {
-
+    PENDING = "pending",
+    IN_PROGRESS = "inProgress",
+    COMPLETED = "completed",
 }
 
-export { Todo, TodoStatus };
+type NewTodo = Omit<Todo, "id" | "createdAt">
+
+export { Todo, NewTodo ,TodoStatus };
